a28b1586c6b85344f3cd8f021cbd60c901f68429
vir-th-w11 <gruningzen202302@gmail.com>
2023-09-21 15:11:37 -0300

    feat: add file for operators

---
+++ src/operators.js
@@ -0,0 +1,16 @@
+var ubuntu = 23281
+var wsl = 13674
+var debian = 7059
+var otherLinux = 6712
+var arch = 3809
+var redHat = 4043
+var fedora = 2662
+var chromeOs = 927
+
+var linux = ubuntu + wsl + debian + otherLinux + arch + redHat + fedora + chromeOs
+
+var windows = 40917
+
+console.log(`${windows}) 
+
+console.log(linux)
