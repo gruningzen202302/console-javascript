93536bc554278f5567656e5c93d24dcc2dd6d156
vir-th-w11 <gruningzen202302@gmail.com>
2023-09-22 02:11:02 -0300

    feat:update code for operators excercise

--- src/advanced-operators.js
+++ src/advanced-operators.js
@@ -1,7 +1,34 @@
 var score = 8 
 console.log('Mid-level skills : ' + (score > 0 && score < 10))
 
-//import jest and write a test for the following code
 var timeRemaining = 0
 var energy = 10
 console.log(`Game over: ${ timeRemaining == 0 || energy == 0 }`)
+
+timeRemaining = 3
+console.log(`Game over: ${ timeRemaining == 0 || energy == 0 }`)
+
+var num1 = 2
+var num2 = 5
+var test1 = num1 % 2
+//console.log('test1 2 mod 2=' + test1 )
+var test2 = num2 % 2
+//console.log('test2 5 mod 2=' +test2)
+var result1 = test1 == 0 
+var result2 = test2 == 0
+console.log( "Is", num1, "an even number?", result1) 
+console.log( "Is", num2, "an even number?", result2 ) 
+
+console.log(10 + 5)
+
+var now = 'Now in '
+var three = 3
+var d = 'D!'
+console.log(now + three + d)
+
+var counter = 0
+counter += 5
+counter += 3
+counter ++
+counter **= 2
+console.log(counter)
