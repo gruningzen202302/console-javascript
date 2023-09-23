179a7b0eac6140e31ce61f57624a5869d589ba8d
vir-mac-pro <alberto.gruning.zen@gmail.com>
2023-09-23 05:19:37 -0300

    feat:update loops for a snippet with formatted console

--- src/loops.js
+++ src/loops.js
@@ -26,3 +26,10 @@ while(year <=2022){
     console.log( year) 
     year++ 
 }
+
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
