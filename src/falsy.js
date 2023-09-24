04477b43c3d2269499f6867f4b1f1cbbde4ae2f3
vir-mac-pro <alberto.gruning.zen@gmail.com>
2023-09-24 03:25:26 -0300

    feat:create file to test falsy statements

---
+++ src/falsy.js
@@ -0,0 +1,22 @@
+
+if(true) {console.log('A')} else console.log('B')
+
+if(null) {console.log('A')} else console.log('B')
+
+if(undefined) {console.log('A')} else console.log('B')
+
+if(false) {console.log('A')} else console.log('B')
+
+if(NaN) {console.log('A')} else console.log('B')
+
+if(0) {console.log('A')} else console.log('B')
+
+if(-0) {console.log('A')} else console.log('B')
+
+if(0n) {console.log('A')} else console.log('B')
+
+if("") {console.log('A')} else console.log('B')
+
+//try this in chrome 
+// if(document.all) {console.log('A')} else console.log('B')
+// document.body.innerHTML = "<h1>HELLO!!!</h1>"
