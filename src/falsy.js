4f6cc8c7d8f81d734d1d7020adbad8f0d3bb27c1
vir-mac-pro <alberto.gruning.zen@gmail.com>
2023-09-24 03:33:50 -0300

    feat: rename fiel to test truthy and falsy expressions

--- src/falsy.js
+++ src/falsy.js
@@ -1,5 +1,22 @@
+console.log('Truthy values')
 
 if(true) {console.log('A')} else console.log('B')
+if (true) {console.log('A')} else console.log('B')
+if ({}) {console.log('A')} else console.log('B')
+if (new Object()) {console.log('A')} else console.log('B')
+if ([]) {console.log('A')} else console.log('B')
+if (42) {console.log('A')} else console.log('B')
+if ("0") {console.log('A')} else console.log('B')
+if ("false") {console.log('A')} else console.log('B')
+if (new Date()) {console.log('A')} else console.log('B')
+if (-42) {console.log('A')} else console.log('B')
+if (12n) {console.log('A')} else console.log('B')
+if (3.14) {console.log('A')} else console.log('B')
+if (-3.14)  {console.log('A')} else console.log('B')
+if (Infinity) {console.log('A')} else console.log('B')
+if (-Infinity) {console.log('A')} else console.log('B')
+
+console.log('Falsy values')
 
 if(null) {console.log('A')} else console.log('B')
 
