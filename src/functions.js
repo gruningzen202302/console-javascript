411349c988d398aa25e4cd2eb83517d64cd81b78
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-10-13 20:05:19 -0300

    docs: add more funcions example to refer the mozilla docs

--- src/functions.js
+++ src/functions.js
@@ -100,3 +100,44 @@ const msg = foo(' ++ ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','
 
 msg
 console.log(msg)
+
+const person = {
+  fullName: {
+      first: "Bob",
+      last: "Smith",
+    },
+  name: ["Bob", "Smith"],
+  age: 32,
+  bio: function () {
+    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  },
+  introduceSelf: function () {
+    console.log(`Hi! I'm ${this.name[0]}.`);
+  },
+};
+
+person
+
+person.bio()
+
+const full = person.fullName.first + " " + person.fullName.last
+
+full
+
+person.farewell = function () {
+  console.log("Bye everybody!");
+}
+
+person.farewell()
+
+let nameInput = ''
+let nameValue = ''
+
+let myDataName = "height";
+let myDataValue = "1.75m";
+
+person[myDataName] = myDataValue;
+
+const ht = person.height
+
+console.log(ht)
