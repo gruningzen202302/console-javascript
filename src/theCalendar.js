9eaedb4f0bf2be5db5fe142cadec5fe03eb3d1d6
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-12-01 23:17:52 -0300

    feat(add code for naive solution): [729] add code for naive solution

--- src/theCalendar.js
+++ src/theCalendar.js
@@ -1,18 +1,54 @@
 var TheCalendar = function () {};
 
+TheCalendar.prototype.bookedEvents = []
+
 /**
- * @param {number} start
- * @param {number} end
+ * @param {number} currentEventStart
+ * @param {number} currentEventEnd
  * @return {boolean}
  */
-TheCalendar.prototype.book = function (start, end) {};
+TheCalendar.prototype.book = function (currentEventStart, currentEventEnd) {
+  console.log(
+    "currentEventStart " +
+      currentEventStart +
+      " currentEventEnd " +
+      currentEventEnd
+  )
 
-var calendar = new TheCalendar();
+  for (let i = 0; i < this.bookedEvents.length; i++) {
+    const bookedEventStart = this.bookedEvents[i][0]
+    const bookedEventEnd = this.bookedEvents[i][1]
+
+    console.log(
+      "\t bookedEventStart " +
+        bookedEventStart +
+        " bookedEventEnd " +
+        bookedEventEnd
+    )
+
+    const conditionsForNextEventStartOverlap =
+      bookedEventStart < currentEventStart && currentEventStart < bookedEventEnd
+
+    const conditionsForPreviusEventOverlap =
+      currentEventStart < bookedEventStart && bookedEventStart < currentEventEnd
+
+    if (conditionsForNextEventStartOverlap || conditionsForPreviusEventOverlap)
+      return false
+  }
+
+  this.bookedEvents.push([currentEventStart, currentEventEnd])
+
+  return true
+}
+
+var calendar = new TheCalendar()
 
 const events = [
-  [10, 20],
-  [15, 25],
-  [20, 30],
-];
+  /**/ [10, /* */ 20],
+  /*      */ [15, /*   */ 25],
+  /*           */ [20, /*   */ 30],
+  [5, /**/ 12],
+]
 
-events.forEach((event) => console.log(calendar.book(event)));
+events.forEach((event) => console.log(calendar.book(event[0], event[1])))
+console.log(calendar.bookedEvents)
